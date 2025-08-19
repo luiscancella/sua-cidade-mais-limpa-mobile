0a2f50e3d6c49eb99d8193de9c1871ebe93f0a28
Luís Cancella <luisfernando@cancella.com.br>
2025-08-19 18:57:52 -0300

    feat: notification configuration screen

--- src/screens/configuration/NotificationConfigScreen.tsx
+++ src/screens/configuration/NotificationConfigScreen.tsx
@@ -1,10 +1,48 @@
-import { Text, } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { SwitchItemList } from "src/components/SwitchItemList";
 
 export function NotificationConfigScreen() {
+    const [notificationEnabled, setNotificationEnabled] = useState(false);
+    const [newsEnabled, setNewsEnabled] = useState(false);
+
+    useEffect(() => {
+        // TODO: Implementar logica de mudança de botão
+        console.log("Mudando o estado da notificação:", notificationEnabled);
+    }, [notificationEnabled]);
+
+    useEffect(() => {
+        // TODO: Implementar logica de mudança de botão
+        console.log("Mudando o estado das notícias:", newsEnabled);
+    }, [newsEnabled]);
+
     return (
-        <SafeAreaView>
-            <Text>Configurações de Notificação</Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.notificationLabel}>Habilitar/desabilitar Notificações</Text>
+            <SwitchItemList
+                text="Previsão de chegada de coleta de lixo"
+                value={notificationEnabled}
+                onValueChange={(value) => setNotificationEnabled(value)}
+            />
+
+            <SwitchItemList
+                text="Notícias e atualizações"
+                value={newsEnabled}
+                onValueChange={(value) => setNewsEnabled(value)}
+            />
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    notificationLabel: {
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 5,
+        marginHorizontal: "auto",
+    }
+});
